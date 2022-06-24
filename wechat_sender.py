107d835281ac83ee4c8450f3e13f295a5ce0bd25
yuzhoukai <yuzhoukai@onchain.com>
2022-06-24 18:43:13 +0800

    touser

--- wechat_sender.py
+++ wechat_sender.py
@@ -4,12 +4,16 @@ import os
 
 wechat_server = os.environ.get('URL')
 bearer = os.environ.get('BEARER')
+touser = os.environ.get('TOUSER')
 
 
 def send_wechat(msg):
     print("Sent wechat...")
+    url = 'http://' + wechat_server
+    if touser:
+        url += '?touser=' + touser
     requests.post(
-        url='http://' + wechat_server,
+        url=url,
         headers={"Authorization": bearer, "Content-Type": "application/json"},
         data=json.dumps({
             "app": "mj",
