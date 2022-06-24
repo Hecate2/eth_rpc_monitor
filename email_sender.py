e1c24f2f3295bd3df076fce3b53f20b78df70815
Hecate2 <2474101468@qq.com>
2022-06-24 17:37:24 +0800

    return if no email_user

--- email_sender.py
+++ email_sender.py
@@ -2,10 +2,14 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 import os
+import sys
 
 
 def send_email(subject: str='测试主题', content: str='测试邮件', smtp_server='smtp.163.com'):
     email_user = os.environ.get('email_user')
+    if not email_user:
+        print('WARNING: NO email_user specified', file=sys.stderr)
+        return 
     password = os.environ.get('password')
     
     email_sender = email_user
